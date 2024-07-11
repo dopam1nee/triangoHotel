e93128706e05e5853a4a2319f50e3d56534019e3
dopa <ISPQ@bk.ru>
2024-07-11 16:06:07 +0200

    booking controller

--- Backend/controllers/booking.js
+++ Backend/controllers/booking.js
@@ -1,13 +1,28 @@
 const Booking = require('../models/Booking')
 const Room = require('../models/Room')
+const User = require('../models/User')
 
-const addBooking = async (roomId, booking) => {
+const addBooking = async (userId, roomId, dates) => {
 	// TODO дописать контроллеры
-	const newBooking = await Booking.create(booking)
+	const newBooking = await Booking.create(dates) // { _id: new ObjectId('...'), checkIn: ..., checkOut: ... }
+	const room = await Room.findByIdAndUpdate(roomId, {
+		$push: { bookings: newBooking },
+	}) // { _id: new ObjectId('...'), number: ..., bookings: [ new ObjectId('...') ] }
 
-	await Room.findByIdAndUpdate(roomId, { $push: { bookings: newBooking._id } }) // или просто newBooking; привязали бронирование к номеру комнаты
+	await User.findByIdAndUpdate(userId, {
+		$push: { rooms: room },
+	})
+	//.populate({
+	//	path: 'rooms',
+	//	select: '-createdAt -updatedAt -__v',
+	//	populate: {
+	//		path: 'bookings',
+	//		select: '-createdAt -updatedAt -__v',
+	//	},
+	//})
+	// или newBooking._id; добавили бронирование в массив забронированных номеров пользователя; раскрыли объект по id; преобразовали идентификатор в объект, в котором содержится информация о пользователе (по идентификатору)
 
-	//await newBooking.populate('room') // раскрыли объект по id; преобразовали идентификатор в объект, в котором содержится информация о пользователе (по идентификатору)
+	//console.log(user.rooms[0].bookings) // User.findByIdAndUpdate занести в константу user, и можно в консоли при бронировании увидеть [ { _id: new ObjectId('...'), checkIn: ..., checkOut: ... }, ... ]
 
 	return newBooking
 }
@@ -24,7 +39,17 @@ const editBooking = async (id, room) => {
 	return newBooking
 }
 
-const deleteBooking = id => Booking.findByIdAndDelete(id)
+const deleteBooking = async (userId, roomId, bookingId) => {
+	await Booking.deleteOne({ _id: bookingId })
+
+	const room = await Room.findByIdAndUpdate(roomId, {
+		$pull: { bookings: bookingId },
+	})
+
+	await User.findByIdAndUpdate(userId, {
+		$pull: { rooms: room._id },
+	})
+}
 
 module.exports = {
 	addBooking,
--- Backend/mappers/map-room.js
+++ Backend/mappers/map-room.js
@@ -1,3 +1,5 @@
+const mapBooking = require('./map-booking')
+
 const mapRoom = room => {
 	return {
 		id: room._id,
@@ -6,6 +8,11 @@ const mapRoom = room => {
 		price: room.price,
 		image: room.image,
 		status: room.status,
+		bookings: room.bookings, // TODO
+		//bookings: room.bookings.map(
+		//	booking =>
+		//		mongoose.isObjectIdOrHexString(booking) ? booking : mapBooking(booking), // проверяем, является ли текущий элемент строкой ObjectId или шестнадцатеричной строкой
+		//),
 	}
 }
 module.exports = mapRoom
